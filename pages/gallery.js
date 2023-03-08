4905b87a939de2268acc24d181bc8f2f6b6ad24c
codyhusek <cody.husek@husoftsolutions.com>
2023-03-08 13:54:52 -0500

    added gallery page

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,12 +1,23 @@
+import config from '@/components/config.json';
 import React from 'react';
+import Image from 'next/image';
 import Layout from '@/components/Layout';
 
-function Gallery() {
+const Gallery = () => {
   return (
     <Layout>
-      <div>Gallery</div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 m-4">
+        {config?.gallery?.map((image, index) => (
+          <div
+            key={index}
+            className="bg-gray-200 h-64 flex items-center justify-center overflow-hidden"
+          >
+            <img src={image} alt="" className="h-full w-full object-cover" />
+          </div>
+        ))}
+      </div>
     </Layout>
   );
-}
+};
 
 export default Gallery;
