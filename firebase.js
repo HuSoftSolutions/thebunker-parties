5a7fffa24a3fb9a6182f8ef5af19b3baeaa8c791
codyhusek <cody.husek@husoftsolutions.com>
2023-04-01 00:57:52 -0400

    fixed gitignore

--- firebase.js
+++ firebase.js
@@ -4,13 +4,13 @@ import { getFunctions, httpsCallable } from 'firebase/functions';
 import { getAuth, onAuthStateChanged, signInAnonymously } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAUOr1bVPL625XjELTaE2-OwInL6lueS-g',
-  authDomain: 'thebunkerparties.firebaseapp.com',
-  projectId: 'thebunkerparties',
-  storageBucket: 'thebunkerparties.appspot.com',
-  messagingSenderId: '804722565276',
-  appId: '1:804722565276:web:d927c1f27ffde106b1cd59',
-  measurementId: 'G-BJCMXC46FL',
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
