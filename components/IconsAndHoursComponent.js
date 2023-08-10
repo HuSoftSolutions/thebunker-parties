9cdc085110282c89e36eaf2d98394b40c240e5df
codyhusek <cody.husek@husoftsolutions.com>
2023-08-10 15:15:29 -0400

    fixed guilderland menu with cater notice

--- components/IconsAndHoursComponent.js
+++ components/IconsAndHoursComponent.js
@@ -8,7 +8,7 @@ const IconsAndHours = ({ locObj, mapClick, mapClose }) => {
   const router = useRouter();
 
   return (
-    <div className="flex flex-col md:flex-row items-center justify-between">
+    <div className="flex flex-col md:flex-row items-center justify-between h-full">
       <div className="flex">
         <IconComponent
           click={mapClick}
@@ -16,14 +16,19 @@ const IconsAndHours = ({ locObj, mapClick, mapClose }) => {
           title="DIRECTIONS"
           icon={<CgPin className="w-full h-full" />}
         />
-        <IconComponent
+       {locObj?.menuPath !== 'hide' ? <IconComponent
           title="MENU"
           icon={<GiKnifeFork className="w-full h-full" />}
           click={() => {
             router.push(locObj.menuPath || "/menu");
           }}
-        />
+        /> : null }
       </div>
+
+			{locObj?.menuPath === 'hide' ? 
+			<div>
+				<div className="text-primary text-xs sm:text-sm bg-red-50 p-3 my-5 rounded text-center font-bold w-full h-full">Outside catering available</div>
+			</div> : null}
 
       <div className="font-normal text-black">
         <div>
